815b4f49a15c617aee19fbaa944d48cdc20111ef
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-09-16 23:30:05 +0200

    changes for vercel to work

--- src/pages/blog/Compartimos.jsx
+++ src/pages/blog/Compartimos.jsx
@@ -1,7 +1,6 @@
-import NavWintek from '../../components/navbar/Navbar';
+import NavWintek from '../../components/navbar/NavbarMenu';
 import Footer from '../../components/footer/Footer';
-import './Compartimos.css';
-import "../../components/card/TextCard.css";
+import styles from './Compartimos.module.css';
 import BlogCard from '../../components/card/BlogCard';
 import BlogCards from '../../config/blogCards.json';
 
@@ -10,7 +9,7 @@ function Compartimos() {
     <div>
         <NavWintek/>
         <div className="cont-main-section">
-            <p className="txt-title-section">COMPARTIMOS CONOCIMIENTO</p>
+            <p className={styles.title}>COMPARTIMOS CONOCIMIENTO</p>
             <div className="cont-main-section-one">
             {
                 BlogCards.map((blogcard, index) => <BlogCard key={index} {...blogcard}/>)
