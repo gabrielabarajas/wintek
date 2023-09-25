b733aa38ef3c65361abbd37cd8f18c8516f6c2d0
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-09-25 12:00:10 +0200

    Navbar fija

--- src/components/navbar/NavbarMenu.jsx
+++ src/components/navbar/NavbarMenu.jsx
@@ -15,7 +15,9 @@ function NavbarMenu() {
   return (
     <>
       {['lg'].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+        <Navbar key={expand} expand={expand} className={`bg-body-tertiary mb-3 ${styles.container}`}>
+
+
           <Container fluid>
           <Image  className= {styles.navbarLogo}src={Logo} alt="logo empresa" width={220} height={60} />
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
@@ -32,12 +34,15 @@ function NavbarMenu() {
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="#inicio" onClick={() => scrollTo('#bienvenidos')}>Inicio</Nav.Link>
-                <Nav.Link href="#avancemos" onClick={() => scrollTo('#avancemos')}>Avancemos juntos</Nav.Link>
+
                 <Nav.Link href="#adnWintek" onClick={() => scrollTo('#adnWintek')}>ADN Wintek</Nav.Link>
-                <Nav.Link href="#" onClick={() => scrollTo('#transformamos')}>¿Que hacemos?</Nav.Link>
-                <Nav.Link href="#iniciaCamino" onClick={() => scrollTo('#iniciaCamino')}>Solicita información</Nav.Link>
+
+                <Nav.Link href="#avancemos" onClick={() => scrollTo('#avancemos')}>Avancemos Juntos</Nav.Link>
+
+                <Nav.Link href="#iniciaCamino" onClick={() => scrollTo('#iniciaCamino')}>Solicita Información</Nav.Link>
+
                 <Nav.Link href="#contactanos" onClick={() => scrollTo('#compartimos')}>Blog</Nav.Link>
-                <Nav.Link href="#contactanos" onClick={() => scrollTo('#contacta')}>Contáctanos</Nav.Link>
+
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
